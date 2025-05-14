8509d33084299ffd394bd4672cc7e84510480414
Arbinxtha <Arbinxtha2020@gmail.com>
2025-05-14 22:14:04 +1000

    Added schema for the schedule notificaitons

---
+++ database/migrations/2025_05_04_164451_create_scheduled_notifications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scheduled_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->json('user_ids'); // Array of selected user IDs
+            $table->text('message');
+            $table->timestamp('scheduled_at');
+            $table->boolean('is_sent')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scheduled_notifications');
+    }
+};
